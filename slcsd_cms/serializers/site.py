1e06f22bd09bb328c8d27fe671ce184bba3e1f7d
iamjdcollins <jd@iamjdcollins.com>
2018-11-30 15:56:39 -0700

    Add in the canonical attributes. It would be nice to show the current environments canonical but since that is a calculated property it results in an additional query per record. Might look for a better solution to show the current site canonical that scales well.

--- slcsd_cms/serializers/site.py
+++ slcsd_cms/serializers/site.py
@@ -16,6 +16,27 @@ class SiteSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True,
         # queryset=Domain.objects.all(),
     )
+    development_canonical = serializers.HyperlinkedRelatedField(
+        many=False,
+        view_name="slcsd_cms:api:domain-detail",
+        lookup_field="pk",
+        lookup_url_kwarg="pk",
+        read_only=True,
+    )
+    testing_canonical = serializers.HyperlinkedRelatedField(
+        many=False,
+        view_name="slcsd_cms:api:domain-detail",
+        lookup_field="pk",
+        lookup_url_kwarg="pk",
+        read_only=True,
+    )
+    production_canonical = serializers.HyperlinkedRelatedField(
+        many=False,
+        view_name="slcsd_cms:api:domain-detail",
+        lookup_field="pk",
+        lookup_url_kwarg="pk",
+        read_only=True,
+    )
     update_date = serializers.DateTimeField(
         required=False,
         format='%b. %d, %Y, %I:%M %p'
@@ -29,6 +50,9 @@ class SiteSerializer(serializers.HyperlinkedModelSerializer):
             'title',
             'description',
             'management',
+            'development_canonical',
+            'testing_canonical',
+            'production_canonical',
             'domains',
             'update_date',
         )
