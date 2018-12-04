204aa2a5bcccae21354d0fc3e30c04cd1475c65f
iamjdcollins <jd@iamjdcollins.com>
2018-12-04 10:13:01 -0700

    Add a uuid field to the group model.

--- slcsd_cms/models/group.py
+++ slcsd_cms/models/group.py
@@ -1,6 +1,13 @@
+from uuid import uuid4
+
 from django.db import models
 from django.contrib.auth.models import Group
 
+Group.add_to_class('uuid',  models.UUIDField(
+    unique=True,
+    default=uuid4,
+    editable=False,
+))
 Group.add_to_class('title', models.CharField(
     null=True,
     blank=False,
