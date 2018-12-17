8eb6b46117d85479fe2a65369ddc5872ba086885
iamjdcollins <jd@iamjdcollins.com>
2018-12-17 15:23:05 -0700

    Figured out how to reload gunicorn automatically in dev so I can now cache templates. Also set the static url.

--- districtwebsites/settings/development.py
+++ districtwebsites/settings/development.py
@@ -13,5 +13,4 @@ ENVIRONMENT = 'DEVELOPMENT'
 # Required for Django Debug Toolbar
 INTERNAL_IPS = '192.168.2.1'
 
-TEMPLATES[0]['APP_DIRS'] = True
-del TEMPLATES[0]['OPTIONS']['loaders']
+STATIC_URL = 'https://websites-dev.slcschools.org/static/'
